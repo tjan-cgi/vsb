08f385a04e88f0ada6896f5ce820f6f811ac60cd
Tomáš Janečko <tomas.janecko@cgi.com>
2025-04-23 10:38:55 +0200

    Test was fixed

--- src/App.tsx
+++ src/App.tsx
@@ -85,13 +85,6 @@ export const App = () => {
           </CardFooter>
         </Card>
 
-        <Alert variant="destructive" className="w-[350px] animate-pulse">
-          <AlertTriangle className="h-4 w-4" />
-          <AlertTitle>Kritická chyba!</AlertTitle>
-          <AlertDescription>
-            Toto je kritická chyba, oprav mne.
-          </AlertDescription>
-        </Alert>
       </div>
     </div>
   );
